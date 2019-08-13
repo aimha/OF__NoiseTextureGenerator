bcc352b511cba3649ed0fbac7adeec99dbaf6b85
Christian <christian.boragine@gmail.com>
2019-08-13 11:24:01 +0200

    Add files via upload

---
+++ bin/data/shaderGL3/fbm.frag
@@ -0,0 +1,39 @@
+// fragment shader
+
+#version 150
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 textureMatrix;
+uniform mat4 modelViewProjectionMatrix;
+
+in vec4 position;
+in vec3 normal;
+in vec4 color;
+in vec2 texcoord;
+
+out vec4 outputColor;
+
+// custom
+
+uniform float red;
+uniform float green;
+uniform float blue;
+
+uniform float time;
+
+float random2D(in vec2 p) {
+  return fract(sin(dot(p, vec2(12.9898, 78.238))) * 43758.5453123);
+}
+
+float random3D(in vec2 p, in float t) {
+  return fract(sin(dot(vec3(p, t), vec3(12.9898, 78.238, t * .0001))) * 43758.5453123);
+}
+
+void main()
+{
+    vec2 st = gl_FragCoord.xy / vec2(1920., 1200.);
+    vec3 color = vec3(random3D(st.xy, time));
+
+    outputColor = vec4(color, 1.);
+}
---
+++ src/ofApp.cpp
@@ -0,0 +1,122 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	
+	// global setup
+	ofBackground(0);
+	ofSetVerticalSync(true);
+
+	// set variables
+	w = ofGetWidth();
+	h = ofGetHeight();
+
+	// shader setup
+	fbmShader.load("shaderGL3/fbm");
+
+	//plane setup
+	plane.set(800, 800);
+	plane.setPosition(0, 0, 0);
+	plane.setResolution(400, 400);
+
+	// fbo setup
+	fbo.allocate(w, h);
+
+	// gui setup
+	gui.setup("Parameters", "settings.xml");
+	gui.add(red.setup("red", 0., 0., 100.));
+	gui.add(green.setup("green", 0., 0., 100.));
+	gui.add(blue.setup("blue", 0., 0., 100.));
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	// open fbo
+	fbo.begin();
+		
+		// start shader
+		fbmShader.begin();
+
+		// pass uniforms to shader
+		fbmShader.setUniform1f("red", red);
+		fbmShader.setUniform1f("green", green);
+		fbmShader.setUniform1f("blue", blue);
+		fbmShader.setUniform1f("time", ofGetElapsedTimef());
+
+		// matrix transformations
+		ofPushMatrix();
+		ofTranslate(w / 2., h / 2.);
+
+		// plane draw
+		plane.draw();
+		
+		// end shader
+		fbmShader.end();
+
+	// close fbo
+	fbo.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	// draw fbo
+	fbo.draw(0, 0, w, h);
+
+	// draw gui
+	gui.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
